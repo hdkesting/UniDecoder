9d004c55ab23f22b9223a1136666a1b22e26b133
Hans Kesting <hdkesting@github.com>
2017-11-30 16:10:30 +0100

    Change to AutoSuggestBox

--- UniDecoderWpf/ViewModels/ShowBlockPageViewModel.cs
+++ UniDecoderWpf/ViewModels/ShowBlockPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Template10.Services.NavigationService;
@@ -23,9 +24,65 @@ namespace UniDecoderWpf.ViewModels
             return this.svc.GetCharactersByBlock(Value);
         }
 
-        public void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //public void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    Value = ((ComboBox)sender).SelectedValue.ToString();
+        //}
+
+        public void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
         {
-            Value = ((ComboBox)sender).SelectedValue.ToString();
+            // Only get results when it was a user typing, 
+            // otherwise assume the value got filled in by TextMemberPath 
+            // or the handler for SuggestionChosen.
+            if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
+            {
+                //Set the ItemsSource to be your filtered dataset
+                IEnumerable<string> names = BlockNames.Where(x => true);
+                foreach (var word in sender.Text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    names = names.Where(n => n.IndexOf(word, StringComparison.OrdinalIgnoreCase) >= 0);
+                }
+
+                names = names.ToList();
+                sender.ItemsSource = names.ToList();
+                if (names.Count() == 1)
+                {
+                    sender.Text = /*Value =*/ names.Single();
+                }
+            }
+        }
+
+
+        public void AutoSuggestBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+        {
+            // Set sender.Text. You can use args.SelectedItem to build your text string.
+            sender.Text = /*Value =*/ args.SelectedItem.ToString();
+        }
+
+
+        public void AutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            if (args.ChosenSuggestion != null)
+            {
+                // User selected an item from the suggestion list, take an action on it here.
+                sender.Text = /*Value =*/ args.QueryText;
+            }
+            else
+            {
+                // Use args.QueryText to determine what to do.
+                IEnumerable<string> names = BlockNames.Where(x => true);
+                foreach (var word in sender.Text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    names = names.Where(n => n.IndexOf(word, StringComparison.OrdinalIgnoreCase) >= 0);
+                }
+
+                names = names.ToList();
+                sender.ItemsSource = names.ToList();
+                if (names.Count() == 1)
+                {
+                    sender.Text = /*Value =*/ names.Single();
+                }
+            }
         }
 
         public override async Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> suspensionState)
