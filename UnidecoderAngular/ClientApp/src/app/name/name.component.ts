dc681f6f0637e759269944aaffb26c3c5c7e7c32
Hans Kesting <hans_kesting@epam.com>
2019-10-31 17:21:40 +0100

    use local functions; use Observables in "name"

--- UnidecoderAngular/ClientApp/src/app/name/name.component.ts
+++ UnidecoderAngular/ClientApp/src/app/name/name.component.ts
@@ -11,9 +11,9 @@ import { UnidecoderService } from '../unidecoder.service';
     styleUrls: ['./name.component.less']
 })
 export class NameComponent implements OnInit {
-    result: Charinfo[];
+    result: Observable<Charinfo[]>;
     activeCallback;
-    searchBox: HTMLElement;
+    searchBox: HTMLInputElement;
     getter$: Observable<Charinfo[]>;
     getting: boolean;
 
@@ -22,34 +22,38 @@ export class NameComponent implements OnInit {
         private unidecoder: UnidecoderService) { }
 
     ngOnInit() {
+        this.searchBox = document.getElementById('searchString') as HTMLInputElement;
+
         // keep reacting to changes in ('name') param
         this.route.queryParamMap.subscribe(parms => {
             const paramname = parms.get('name');
             if (paramname) {
                 console.log("got NEW name param: '" + paramname + "'")
-                this.findCharacters(paramname);
+                this.searchBox.value = paramname;
+                this.result = this.findCharacters(paramname);
             }
         });
 
-        this.searchBox = document.getElementById('searchString');
-        console.log("Got searchbox? " + this.searchBox);
-
-        this.getter$ = fromEvent(this.searchBox, 'input').pipe(
+        this.result = fromEvent(this.searchBox, 'input').pipe(
             map((e: KeyboardEvent) => {
                 return (e.target as HTMLInputElement).value;
             }),
-            filter(text => text.length > 1),
             debounceTime(500),
             distinctUntilChanged(),
             flatMap((text: string) => this.findCharacters(text)),
           );
 
-        this.getter$.subscribe(data => {
-            this.getting = false;
-            console.log("got " + data.length + " characters");
-            this.result = data;
-        });
-     }
+        //this.getter$.subscribe(this.processResults);
+    }
+
+    /*
+    private processResults(data: Charinfo[]) {
+        this.getting = false;
+        this.result = data;
+
+        console.log("got " + this.result.length + " characters");
+    }
+    */
 
     findCharacters(text: string): Observable<Charinfo[]> {
         console.log('getting characters for "' + text + '"');
