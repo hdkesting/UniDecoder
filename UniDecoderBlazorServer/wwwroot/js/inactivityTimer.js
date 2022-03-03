2b47c3288ac484fc14e49700fc7c4b467491b990
Hans Kesting <hans_kesting@hotmail.com>
2022-03-03 12:15:30 +0100

    jump out of Blazor app after a period of inactivity

---
+++ UniDecoderBlazorServer/wwwroot/js/inactivityTimer.js
@@ -0,0 +1,16 @@
+﻿export function initializeInactivityTimer() {
+    var timer;
+
+    document.onmousemove = resetTimer;
+    document.onkeypress = resetTimer;
+    document.onscroll = resetTimer; // scrolling by mouse-wheel doesn't count as mousemove
+
+    function resetTimer() {
+        clearTimeout(timer);
+        timer = setTimeout(logout, 10 * 60 * 1000);
+    }
+
+    function logout() {
+        document.location = "./loggedout.html"
+    }
+}
