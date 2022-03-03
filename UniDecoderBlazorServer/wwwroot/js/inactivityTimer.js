d99ba93e846c7d73c58a9bb86501a563f4bdc642
Hans Kesting <hans_kesting@hotmail.com>
2022-03-03 13:20:05 +0100

    tweaks to auto-logout

--- UniDecoderBlazorServer/wwwroot/js/inactivityTimer.js
+++ UniDecoderBlazorServer/wwwroot/js/inactivityTimer.js
@@ -5,9 +5,12 @@
     document.onkeypress = resetTimer;
     document.onscroll = resetTimer; // scrolling by mouse-wheel doesn't count as mousemove
 
+    resetTimer(); // SET the timer initially, so that it even works if you start the app and do nothing
+
     function resetTimer() {
         clearTimeout(timer);
         timer = setTimeout(logout, 10 * 60 * 1000);
+        //timer = setTimeout(logout, 3000);
     }
 
     function logout() {
