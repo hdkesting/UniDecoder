332c5f0e2ddc15f4ac80e0c60ff46426071231cc
Hans Kesting <hans_kesting@hotmail.com>
2022-05-16 17:39:51 +0200

    handle letters-plus-accents

---
+++ UniDecoderBlazorServer/Models/StringElement.cs
@@ -0,0 +1,13 @@
+﻿namespace UniDecoderBlazorServer.Models;
+
+public class StringElement
+{
+    public string Element { get; set; }
+
+    public List<CodepointInfo> Codepoints { get; set; } = new List<CodepointInfo>();
+
+    public StringElement(string element)
+    {
+        Element = element;
+    }
+}
