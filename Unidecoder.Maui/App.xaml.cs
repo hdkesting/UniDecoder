012554fad26b92ba4a3508100fe1dfb698ec1b4d
Hans Kesting <Hans.Kesting@salesmanager.nl>
2022-09-15 14:32:43 +0200

    add basic project

---
+++ Unidecoder.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Unidecoder.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
