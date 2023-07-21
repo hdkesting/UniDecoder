593f458e4a6f9c81fab5888c850d92d7ff27ffe1
Hans Kesting <hans_kesting@hotmail.com>
2023-07-21 18:11:42 +0200

    try position-specific templates; 'SingleElement' is still failing

---
+++ Unidecoder.Maui/Controls/SingleElement.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace Unidecoder.Maui.Controls;
+
+using Unidecoder.Maui.Models;
+
+public partial class SingleElement : ContentView
+{
+    public static readonly BindableProperty ElementProperty
+        = BindableProperty.Create(nameof(Element), typeof(CodepointInfo), typeof(SingleElement));
+
+    public SingleElement()
+    {
+    }
+
+    public CodepointInfo Element
+    {
+        get => (CodepointInfo)GetValue(ElementProperty);
+        set => SetValue(ElementProperty, value);
+    }
+}
