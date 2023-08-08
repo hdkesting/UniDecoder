61ae0ebbbbab5fc2385009295c275e83c8bc13b5
Hans Kesting <hans_kesting@hotmail.com>
2023-08-08 11:08:34 +0200

    update inactivityTimer; deploy

--- UniDecoderBlazorServer/wwwroot/js/inactivityTimer.js
+++ UniDecoderBlazorServer/wwwroot/js/inactivityTimer.js
@@ -1,19 +1,43 @@
 ﻿export function initializeInactivityTimer() {
-    var timer;
+    const minutes = 10;
+    let isActivityDetected = false;
+    let minuteCounter = 0;
 
-    document.onmousemove = resetTimer;
-    document.onkeypress = resetTimer;
-    document.onscroll = resetTimer; // scrolling by mouse-wheel doesn't count as mousemove
+    document.onmousemove = activityDetected;
+    document.onkeydown = activityDetected;
+    document.onscroll = activityDetected; // scrolling by mouse-wheel doesn't count as mousemove, so catch separately
 
-    resetTimer(); // SET the timer initially, so that it even works if you start the app and do nothing
+    // check every minute for recent activity
+    let timer = setInterval(checkActivity, 1 * 60 * 1000); // 1 minute
+    console.log("InactivityTimer: Will log out after " + minutes + " minutes of inactivity");
 
-    function resetTimer() {
-        clearTimeout(timer);
-        timer = setTimeout(logout, 10 * 60 * 1000);
-        //timer = setTimeout(logout, 3000);
+    // just quickly set a flag on detected activity
+    function activityDetected() {
+        isActivityDetected = true;
+    }
+
+    function checkActivity() {
+        if (isActivityDetected) {
+            // activity seen, so reset all
+            console.log("InactivityTimer: activity was seen in previous minute")
+            isActivityDetected = false;
+            minuteCounter = 0;
+        } else {
+            // no activity in *at least* the last minute
+            minuteCounter++;
+            console.log(`InactivityTimer: NO activity seen for ${minuteCounter} minute${minuteCounter == 1 ? '' : 's'} out of ${minutes}.`);
+            if (minuteCounter >= minutes) {
+                logout();
+            }
+        }
     }
 
     function logout() {
-        document.location = "./loggedout.html"
+        clearTimeout(timer);
+        console.log("timeout happened - exiting app");
+        let backlen = history.length;
+        console.log(`clearing ${backlen - 1} history items`);
+        history.go(-(backlen - 1)); // keep the last page, which will be replaced
+        setTimeout(() => { history.replaceState(null, '', './loggedout.html'); history.go(0); }, 500);
     }
 }
